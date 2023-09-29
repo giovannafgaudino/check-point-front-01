98b0104500ae6c61b1780b0236d5d9da4f2fe2e7
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-29 08:51:44 -0300

    Adicionando arquivos relacionados ao CP

---
+++ script.js
@@ -0,0 +1,31 @@
+//exercícios 5 e 6-
+let nome ='Giovanna Franco';
+console.log(nome);
+let idade = '18 anos';
+console.log(idade);
+let status = true;
+console.log(status); 
+let valorProduto = 10.50; 
+console.log(valorProduto);
+let listaTimes = ['Palmeiras', 'Boca Juniors', 'Barcelona'];
+console.log('Times escolhidos: ', listaTimes);
+
+// exercícios 7 e 8- 
+let n1= 500;
+let n2= 700;
+let n3= 100;
+let n4= 50;
+let n5= 20;
+// soma:
+let soma = n1 + n2;
+console.log('O resultado de 500 + 700 =', soma);
+//subtração
+let subtracao = n2 - n3;
+console.log('O resultado de 700 - 100 =', subtracao);
+// multiplicação 
+let multiplicacao = n4*n5;
+console.log('O resultado de 50 x 20 = ', multiplicacao);
+// divisão
+let divisao = n2/n5;
+console.log('O resultado de 700 / 20 = ', divisao);
+
